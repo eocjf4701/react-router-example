ef1621adc95c634740c30c415cbd862109261f2f
parkdaechul <eocjf4701@gmail.com>
2024-04-21 23:26:27 +0900

    react-router 적용

---
+++ src/page/Aboutpage.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Aboutpage = () => {
+  return (
+    <div>
+      <h1>
+        Aboutpage
+      </h1>
+    </div>
+  )
+}
+
+export default Aboutpage
---
+++ src/page/Homepage.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Homepage = () => {
+  return (
+    <div>
+      <h1>
+        HomePage
+      </h1>
+    </div>
+  )
+}
+
+
+export default Homepage
